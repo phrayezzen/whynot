d5dfd1ad00be2c70da890dc610d5fdcaba1ecc5c
Xilin Liu <xal1@rice.edu>
2014-03-09 04:17:45 -0500

    init commit

---
+++ src/whynot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whynot::Application.config.secret_key_base = '2cc3f8c1b2c38f5fe04e33a1d8f3c83866788c6c175ad28ae3b21f3fd62d191b351319e8e0820b0f12ab129cc898ce3c11204d8c42d6fdfcd26a6290dcb89a4e'
